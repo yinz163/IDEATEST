66ab2cb3f8cc4fe8818ae2200bec96448ee24fd5
yinz <yinz163@163.com>
2021-01-12 11:21:49 +0800

    第一次提交

---
+++ manager-service/src/main/java/com/yinz/App.java
@@ -0,0 +1,13 @@
+package com.yinz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
